9f1831656c1e909a2e43d7de6e53c9d8d57b69d8
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-31 10:09:59 +0200

    Requiring the messages module and get route to index, where the messages are renderes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+const messages = require('../data/messages');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Mini Message Board',
+    header: 'Messages',
+    messages,
+  });
 });
 
 module.exports = router;
