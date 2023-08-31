2f3bec7ca548d5635e0a4310c2d788dd9f728bb4
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-31 10:10:31 +0200

    Routes for getting the form for getting and posting a new message

--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const messages = require('../data/messages');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.render('form.ejs', { title: 'New message', header: 'Add a new message' });
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  const newMessage = {
+    text: body.message,
+    user: body.user,
+    added: new Date(),
+  };
+
+  messages.push(newMessage);
+  res.redirect('/');
 });
 
 module.exports = router;
