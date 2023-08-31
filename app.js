6c33763bdc34dd68bed827732ba183e9a1eb17e0
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-31 10:09:02 +0200

    Changed var to const for variables, added console.log when the server is running

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var newRouter = require('./routes/new');
+const indexRouter = require('./routes/index');
+const newRouter = require('./routes/new');
 
-var app = express();
+const app = express();
+const PORT = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -37,5 +34,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
 
 module.exports = app;
